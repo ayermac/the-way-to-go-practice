33129a89c961da3b2078f95bea9037c84105360b
jason <codemac@163.com>
2020-07-05 22:51:17 +0800

    新增函数

---
+++ chapter6/6.4/defer.go
@@ -0,0 +1,34 @@
+/**
+ * 关键字 defer 允许我们推迟到函数返回之前（或任意位置执行 return 语句之后）一刻才执行某个语句或函数（为什么要在返回之后才执行这些语句？因为 return 语句同样可以包含一些操作，而不是单纯地返回某个值）
+ */
+package main
+import "fmt"
+
+func main() {
+	function1()
+	a()
+	f()
+}
+
+func function1() {
+	fmt.Printf("In function1 at the top\n")
+	defer function2()
+	fmt.Printf("In function1 at the bottom!\n")
+}
+
+func function2() {
+	fmt.Printf("function2: Deferred until the end of the calling function!\n")
+}
+
+func a() {
+	i := 0
+	defer fmt.Println(i)
+	i++
+	return
+}
+
+func f() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("%d ", i)
+	}
+}
